c646d0233dfbd90354e488a57a88d16038da89ac
teddybearwork <teddybusinessofficial@gmail.com>
2025-09-11 16:13:18 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -175,7 +175,7 @@ const SavingsCalculator = () => {
                 className="p-6 border border-orange-300 rounded-lg hover:border-orange-400 transition-colors text-left"
               >
                 
-                <h3 className="text-lg font-medium text-gray-900 mb-2"><strong className="text-orange-600">Wet Gypsum</strong>Underlayment</h3>
+                <h3 className="text-lg font-medium text-gray-900 mb-2"><strong className="text-orange-600">Wet Gypsum </strong>Underlayment</h3>
                 <p className="text-gray-600 text-sm mb-2">
                   Replace OSB + MAXTERRA Underlayment
                 </p>
